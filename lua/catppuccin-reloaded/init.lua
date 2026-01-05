94daad5a8eeeb9469c7b1caa3bb265c81ced2055
Diego Casella <polentino911@gmail.com>
2026-01-05 22:38:24 +0100

    fix: make defaults explicits

--- lua/catppuccin-reloaded/init.lua
+++ lua/catppuccin-reloaded/init.lua
@@ -3,8 +3,8 @@ local CatppuccinReloaded = {}
 function CatppuccinReloaded.setup(opts)
   local catppuccin = require("catppuccin")
   local paths = vim.api.nvim_get_runtime_file("lua/catppuccin/palettes", true)
+  local seen = { latte = true, frappe = true, macchiato = true, mocha = true}
   local offset = 4
-  local seen = {}
 
   for _, dir in ipairs(paths) do
     local files = vim.fn.readdir(dir)
